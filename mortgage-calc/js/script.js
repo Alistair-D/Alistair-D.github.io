0780e8e889ba971ec575bc5ec5873501e10f5465
Alistair Dickman <dickman.a1@stu.janesville.k12.wi.us>
2025-04-25 14:34:19 -0500

    4/25/2025

--- mortgage-calc/js/script.js
+++ mortgage-calc/js/script.js
@@ -1,7 +1,19 @@
 function calculate() {
-    const P = number(document.getElementById('principal').value);
-    const r = number(document.getElementById('rate').Value)/100;
-    const n = number(document.getElementById('payments').value);
-    const t = number(document.getElementById('years').value);
-    const m = number(document.getElementById('payment').value);
+    let pr = document.getElementById('pr').value;
+    let ra =  document.getElementById('ra').value;
+    let t =  document.getElementById('t').value;
+    let mp = document.getElementById('mp').value;
+
+
+    pr = Number(pr)
+    ra = Number(ra)/100
+    t = Number(t)
+    mp = Number(mp)
+
+
+    b = 0;
+    b = (pr*(1 + ra/12)**(12*t)) - mp*(Math.abs((1 + ra/12)**(12*t)-1)/(ra/12));
+    b = b.toFixed(2)
+    document.getElementById('ba').value = "$" + b;
+
 }
