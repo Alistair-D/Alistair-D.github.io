d61ac64c9ddd33b4f70ba7ac4e6b4cfb28bdfe2d
Alistair Dickman <dickman.a1@stu.janesville.k12.wi.us>
2025-04-28 14:19:57 -0500

    4/28/2025

--- mortgage-calc/js/script.js
+++ mortgage-calc/js/script.js
@@ -3,16 +3,35 @@ function calculate() {
     let ra =  document.getElementById('ra').value;
     let t =  document.getElementById('t').value;
     let mp = document.getElementById('mp').value;
+ 
+ 
+    pr = Number(pr)
+    ra = Number(ra)/100
+    t = Number(t)
+    mp = Number(mp)
+ 
+ 
+    b = 0;
+    b = (pr*(1 + ra/12)**(12*t)) - mp*((1 + ra/12)**(12*t)-1)/(ra/12);
+    document.getElementById("output").innerHTML = "$" + b.toFixed(2);
+ }
 
 
-    let pr = Number(pr)
-    let ra = Number(ra)/100
-    let t = Number(t)
-    let mp = Number(mp)
+ function reset() {
+    let pr = document.getElementById('pr').value;
+    let ra =  document.getElementById('ra').value;
+    let t =  document.getElementById('t').value;
+    let mp = document.getElementById('mp').value;
+
+    pr = Number(pr)
+    ra = Number(ra)/100
+    t = Number(t)
+    mp = Number(mp)
 
 
-    let b = 0;
-    let b = (pr*(1 + ra/12)**(12*t)) - mp*(((1 + ra/12)**(12*t)-1)/(ra/12));
-    document.getElementById('ba').value = "$" + b.toFixed(2);
-
-}
+    document.getElementById('output').innerHTML = "";
+    document.getElementById('pr').value = "";
+    document.getElementById('ra').value = "";
+    document.getElementById('t').value = "";
+    document.getElementById('mp').value = "";
+ }
