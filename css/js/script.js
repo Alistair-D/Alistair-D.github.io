51d86b2a9850bfce7e708494550a5ef8a8db1e72
Alistair Dickman <dickman.a1@stu.janesville.k12.wi.us>
2025-06-05 11:28:28 -0500

    Last Touches

--- css/js/script.js
+++ css/js/script.js
@@ -1,20 +1,7 @@
-<<<<<<< HEAD
-document.getElementById('DownArrow2').addEventListener('click', function() {
+document.getElementById('DownArrow1').addEventListener('click', function() {
 window.scrollBy({
-top: 699,
+top: 759,
 left: 0,
 behavior: 'smooth'
 });
 });
-=======
-
-document.getElementById('DownArrow1').addEventListener('click', function() {
-window.scrollBy({
-top: 1000, 
-left: 0,
-behavior: 'smooth' 
-});
-});
-
-
->>>>>>> 196cc242ba38c29418503d631d1d32efc9bf0c27
--- css/js/script2.js
+++ css/js/script2.js
@@ -1,13 +1,7 @@
 document.getElementById('DownArrow3').addEventListener('click', function() {
 window.scrollBy({
-<<<<<<< HEAD
-top: 999,
-left: 0,
-behavior: 'smooth'
-=======
 top: 999, 
 left: 0,
 behavior: 'smooth' 
->>>>>>> 196cc242ba38c29418503d631d1d32efc9bf0c27
 });
 });
