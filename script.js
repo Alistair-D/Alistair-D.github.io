7bdd0f666b4b437d76c5c133ca8442a79a1621b8
Alistair-D <dickman.a1@stu.janesville.k12.wi.us>
2025-06-04 21:09:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-
 document.getElementById('DownArrow1').addEventListener('click', function() {
 window.scrollBy({
-top: 500, 
+top: 1000, 
+left: 0,
+behavior: 'smooth' 
+});
+});
+
+
+document.getElementById('DownArrow2').addEventListener('click', function() {
+window.scrollBy({
+top: 900, 
+left: 0,
+behavior: 'smooth' 
+});
+});
+
+document.getElementById('DownArrow3').addEventListener('click', function() {
+window.scrollBy({
+top: 1000, 
 left: 0,
 behavior: 'smooth' 
 });
